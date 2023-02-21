914d7745ccc945f004031d5f8ea14e3c7ceae9f3
Yussef raouf <yussef.raouf11@gmail.com>
2023-02-21 03:19:15 +0200

    flask app

---
+++ main/main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_cors import CORS
+from sqlalchemy import UniqueConstraint
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/main'
+CORS(app)
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement = False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name = 'user_product_unique')
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
